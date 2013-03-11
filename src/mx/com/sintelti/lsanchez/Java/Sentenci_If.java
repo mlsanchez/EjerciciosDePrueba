0b1e64f0214785f99199506664856de463e8786c
Lucero <lsanchez@sintelti.com.mx>
2013-03-11 15:10:22 -0600

    Sentencias_if

---
+++ src/mx/com/sintelti/lsanchez/Java/Sentenci_If.java
@@ -0,0 +1,30 @@
+package mx.com.sintelti.lsanchez.Java;
+
+public class Sentenci_If {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var=6;
+		boolean cond;
+		cond = true;
+		if (var <= 10){
+			cond = true;
+			
+		}
+		else {
+			cond = false;
+			
+		}
+		if (cond){
+			System.out.print("Tu operacion es verdadera");
+		}
+		else {
+			System.out.print("Tu operacion es falsa ");
+		}
+
+	}
+
+}

6f1c226ce0ee9a2804a0a5b5c1b444f4e5b3baea
Lucero <lsanchez@sintelti.com.mx>
2013-03-11 12:45:19 -0600

    Representacion de variables y sencillas operaciones matematicas

---
+++ src/mx/com/sintelti/lsanchez/Java/VariablesYOperacionesAritmetivcas.java
@@ -0,0 +1,24 @@
+package mx.com.sintelti.lsanchez.Java;
+
+public class VariablesYOperacionesAritmetivcas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int suma;
+		short resta;
+		float multiplicacion;
+		double divicion;
+		char mensaje;
+		
+		System.out.println("El resultado de sumar 300 + 400 es " + (suma = 300+400));
+		System.out.println("Resta 9083 -8083 " +(resta = 9083 - 8083));
+		System.out.println("Multiplicacion  9.083  * 8.083 " +(multiplicacion = 9083 * 8083));
+		System.out.println("divicion  9083 / 83  = " +(divicion = 9083 / 83));
+		System.out.println("char " +( mensaje =90*10 ));
+
+	}
+
+}
